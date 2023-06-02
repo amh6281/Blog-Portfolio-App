3fe6cfbb6fce299afc692f085546f17d9111f0d5
amh6281 <amh6281@naver.com>
2023-06-02 19:13:48 +0900

    Contact page loading 처리 2022.06.02
    Next.js에서 제공하는 loading을 통해 loading 처리를 구현한다.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div>©2023 MyWeB. All rights reserved.</div>
+    </div>
+  );
 };
 
 export default Footer;
