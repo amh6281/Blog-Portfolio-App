caab5c9badd257113a589527577a192a39a2dab2
amh6281 <amh6281@naver.com>
2023-06-03 13:01:08 +0900

    Home design 2022.06.03

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,17 @@ import Hero from "public/hero.png";
 
 export default function Home() {
   return (
-    <div>
-      <Image src={Hero} alt="MyWeB Hero" className={styles.img} />
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>나만의 포트폴리오</h1>
+        <p className={styles.desc}>
+          MyWeB이 제공하는 감각적인 디자인과 다양한 기능
+        </p>
+        <button className={styles.button}>작업물 보러가기</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="MyWeB Hero" className={styles.img} />
+      </div>
     </div>
   );
 }
