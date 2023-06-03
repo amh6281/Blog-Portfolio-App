325c5c455f6542e11eff97ab65e81cfbee4d710c
amh6281 <amh6281@naver.com>
2023-06-03 11:42:24 +0900

    Footer, Home image 삽입 2022.06.03
    Next.js는 외부 image 주소를 가져올 수 없어 config.js에 해당 웹사이트 주소를 추가하거나 public 폴더에서 image를 저장하고 import하여 사용한다.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2023 MyWeB. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="MyWeB Facebook"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="MyWeB Instagram"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="MyWeB Twitter"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="MyWeB Youtube"
+        />
+      </div>
     </div>
   );
 };
