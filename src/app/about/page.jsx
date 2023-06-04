2f4c6122766018a294e0d96494ce444208d2de35
amh6281 <amh6281@naver.com>
2023-06-04 09:59:49 +0900

    About page 구성요소 추가 2022.06.04

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          fill={true}
+          alt=""
+          className={styles.img}
+        />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>나만의 포트폴리오</h1>
+          <h2 className={styles.imgDesc}>
+            MyWeB이 제공하는 공간에서 포트폴리오를 만들 수 있습니다.
+          </h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Are we?</h1>
+          <p className={styles.desc}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            omnis minus quos obcaecati reprehenderit aliquid quam rerum. Debitis
+            iure, fuga incidunt voluptatem eaque obcaecati, blanditiis voluptate
+            cupiditate commodi excepturi quaerat.
+          </p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>what we do?</h1>
+          <p className={styles.desc}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            omnis minus quos obcaecati reprehenderit aliquid quam rerum. Debitis
+            iure, fuga incidunt voluptatem eaque obcaecati, blanditiis voluptate
+            cupiditate commodi excepturi quaerat.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
