26389ead818d60addb31689685a54a03418621ec
amh6281 <amh6281@naver.com>
2023-06-05 11:02:45 +0900

    Fortfolio page design 2022.06.05

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div>
+      <h1 className={styles.selectTitle}>갤러리 선택</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>일러스트</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>웹사이트</span>
+        </Link>
+        <Link href="/portfolio/applications" className={styles.item}>
+          <span className={styles.title}>애플리케이션</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
