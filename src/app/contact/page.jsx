190fe6b4cf751faaf718e654f1cb2e334209bbc0
amh6281 <amh6281@naver.com>
2023-06-05 10:28:00 +0900

    Contact page design 2022.06.05

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>문의하기</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols="30"
+            rows="10"
+          />
+          <Button url="#" text="문의하기" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
