50e6b94090fb15c5c3f94452f93d271d21af64b3
amh6281 <amh6281@naver.com>
2023-06-06 12:21:53 +0900

    Fortfolio category page 구성요소 추가 2022.06.06

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import styles from "./page.module.css";
+import Button from "@/components/button/Button";
+import Image from "next/image";
 
 const Category = ({ params }) => {
   return (
     <div>
-      <h1>{params.category}</h1>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
