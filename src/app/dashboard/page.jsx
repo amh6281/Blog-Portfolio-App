24c3eb7aa5ce715112f6919feb668a7bc4878c50
amh6281 <amh6281@naver.com>
2023-06-07 11:31:18 +0900

    BClient side data fetching using swr 2022.06.07
    server-state fetching 라이브러리인 swr을 사용하여 client side에서 data를 fetching

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import useSwr from "swr";
 
 const Dashboard = () => {
+  // const [data, setData] = useState([]);
+  // const [error, setError] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store",
+  //     });
+  //     if (!res.ok) {
+  //       setError(true);
+  //     }
+  //     setData(await res.json());
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // }, []);
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSwr(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+  console.log(data);
   return <div>Dashboard</div>;
 };
 
