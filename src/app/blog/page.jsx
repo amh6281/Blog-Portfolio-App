ae393078140bee0c28efbe40b01c2e6749e24b4f
amh6281 <amh6281@naver.com>
2023-06-07 09:56:22 +0900

    Blog page data fetch 2022.06.07
    Next.js의 Dynamic data fetching을 위해 cache no-store 속성을 저장하고, data는 jsonplaceholder에서 가져온다.

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,54 +3,42 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store", // Dynamic data fetching
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
+
   return (
     <div>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>desc</p>
-        </div>
-      </Link>
+      {data.map((item) => (
+        <Link
+          href={`/blog/${item.id}`}
+          className={styles.container}
+          key={item.id}
+        >
+          <div className={styles.imgContainer}>
+            <Image
+              src="https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.body}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
