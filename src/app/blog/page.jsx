e767bfeb1ba2538dd0ef2fd787e9129e5f2eafdf
amh6281 <amh6281@naver.com>
2023-06-08 11:25:07 +0900

    Blog page post api fetch 2022.06.08

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,12 +4,14 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    cache: "no-store", // Dynamic data fetching
+  const res = await fetch("http://localhost:3000/api/posts", {
+    cache: "no-store",
   });
+
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
+
   return res.json();
 }
 
