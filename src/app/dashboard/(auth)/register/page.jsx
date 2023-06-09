60bab38f5b3d0e5f52d3f843b12e46fe1626ec3b
amh6281 <amh6281@naver.com>
2023-06-09 14:01:03 +0900

    Register page design 2022.06.09

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,7 +1,41 @@
+"use client";
+
 import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input
+          type="text"
+          placeholder="username"
+          className={styles.input}
+          required
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className={styles.input}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className={styles.input}
+          required
+        />
+        <button className={styles.button}>회원가입</button>
+      </form>
+      <div className={styles.loginForm}>
+        <span className={styles.span}>이미 회원이신가요?</span>
+        <Link href="/dashboard/login" className={styles.link}>
+          로그인
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
