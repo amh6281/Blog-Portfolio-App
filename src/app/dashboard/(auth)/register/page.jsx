f61104c9703d3da74b481771384131d4a6bac0c6
amh6281 <amh6281@naver.com>
2023-06-09 14:11:29 +0900

    Register page 회원가입 기능 구현 2022.06.09
    fetch 함수를 통해 method, headers, body를 설정하여 회원가입 기능을 구현한다.

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,13 +1,38 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Register = () => {
+  const [error, setError] = useState(false);
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[0].value;
+    const password = e.target[0].value;
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", // JSON format 사용
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
+      res.status === 201 && router.push("/dashboard/login");
+    } catch (err) {
+      setError(true);
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="username"
@@ -28,6 +53,7 @@ const Register = () => {
         />
         <button className={styles.button}>회원가입</button>
       </form>
+      {error && "잘못된 접근입니다."}
       <div className={styles.loginForm}>
         <span className={styles.span}>이미 회원이신가요?</span>
         <Link href="/dashboard/login" className={styles.link}>
