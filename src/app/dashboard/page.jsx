38c4b98ba0835fd23f555d85c193847f28293620
amh6281 <amh6281@naver.com>
2023-06-11 10:54:27 +0900

    Dashboard page 구성 및 post 생성 기능 구현 2022.06.11

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,6 +4,8 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React, { useState, useEffect } from "react";
 import useSwr from "swr";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const Dashboard = () => {
   // const [data, setData] = useState([]);
@@ -41,8 +43,60 @@ const Dashboard = () => {
     router?.push("/dashboard/login");
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          desc,
+          img,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   if (session.status === "authenticated") {
-    return <div></div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {isLoading
+            ? "loading..."
+            : data?.map((post) => (
+                <div className={styles.post} key={post._id}>
+                  <div className={styles.imgContainer}>
+                    <Image src={post.img} alt="" />
+                  </div>
+                  <h2 className={styles.postTitle}>{post.title}</h2>
+                  <span className={styles.delete}>X</span>
+                </div>
+              ))}
+        </div>
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input type="text" placeholder="Desc" className={styles.input} />
+          <input type="text" placeholder="Image" className={styles.input} />
+          <textarea
+            placeholder="Content"
+            className={styles.textArea}
+            cols="30"
+            rows="10"
+          />
+          <button className={styles.button}>Send</button>
+        </form>
+      </div>
+    );
   }
 };
 
