cb42b1831ec10ceb3252a49d0ca62206d9dc0ef2
amh6281 <amh6281@naver.com>
2023-06-22 11:35:29 +0900

    Portfolio model 생성 2022.06.22

---
+++ src/models/Portfolio.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const portfolioSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Portfolio ||
+  mongoose.model("Portfolio", portfolioSchema);
