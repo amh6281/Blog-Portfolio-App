475c3be164e52da2fda8ae53d38f928c85cca0ec
amh6281 <amh6281@naver.com>
2023-06-24 10:23:06 +0900

    Get portfolios api 생성 2022.06.24

--- src/app/api/portfolios/route.js
+++ src/app/api/portfolios/route.js
@@ -3,15 +3,10 @@ import connect from "@/utils/db";
 import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
-  const q = req.query;
-  const filters = {
-    ...(q.category && { category: q.category }),
-  };
-
   try {
     await connect(); // DB 연결
 
-    const portfolios = await Portfolio.find(filters);
+    const portfolios = await Portfolio.find();
 
     return new NextResponse(JSON.stringify(portfolios), { status: 200 });
   } catch (err) {
